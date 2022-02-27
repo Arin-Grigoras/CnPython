c4a1d21cabab9915bd8bacae2fa450ae24e99adb
koreaneggroll <aringrigoras@gmail.com>
2022-02-27 16:29:47 +0200

    binary search tree header file

---
+++ cnpython/headers/datatypes/binarySearchTree/cnpy_BinarySearchTree.h
@@ -0,0 +1,25 @@
+#ifndef CNPY_BINARY_SEARCH_TREE
+#define CNPY_BINARY_SEARCH_TREE
+
+
+struct BinarySearchTree
+{
+    struct Node *head;
+
+    int (*compare)(void *data1, void *data2);
+    void * (*search)(struct BinarySearchTree *tree, void *data);
+    void (*insert)(struct BinarySearchTree *tree, void *data);
+};
+
+/*BinarySearchTree constructor*/
+struct BinarySearchTree binary_search_tree_constructor(int (*compare)(void *data1, void *data2));
+
+
+/*Common things people might want to compare*/
+int binary_search_tree_int_compare(void *data_one, void *data_two);
+int binary_search_tree_float_compare(void *data_one, void *data_two);
+int binary_search_tree_char_compare(void *data_one, void *data_two);
+int binary_search_tree_str_compare(void *data_one, void *data_two);
+
+
+#endif //CNPY_BINARY_SEARCH_TREE
