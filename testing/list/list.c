4d7f723b26a3d64238ffd5056d9ca37630e35db0
koreaneggroll <aringrigoras@gmail.com>
2022-02-27 20:32:29 +0200

    removed uneccesary file

---
+++ testing/list/list.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef char* string;
+
+
+/*
+
+How about no for now
+
+struct List
+{
+    int type;
+
+    string *str;
+    int strlen;
+
+    double *num;
+    int doublelen;
+
+};
+*/
+
+int main(void)
+{
+
+}
